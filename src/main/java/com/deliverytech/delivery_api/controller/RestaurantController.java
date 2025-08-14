9bef2818359fae345e1a7dbf6b27cccb1eff19ea
Juan Lima <juangustavo@gmail.com>
2025-08-14 19:29:14 -0300

    fix: RestaurantController

--- src/main/java/com/deliverytech/delivery_api/controller/RestaurantController.java
+++ src/main/java/com/deliverytech/delivery_api/controller/RestaurantController.java
@@ -1,16 +1,29 @@
 package com.deliverytech.delivery_api.controller;
 
 import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.deliverytech.delivery_api.dto.RestaurantDto;
 import com.deliverytech.delivery_api.service.RestaurantService;
 
+@CrossOrigin(origins = "*")
+@RequestMapping("api/restaurants")
+@RestController
 public class RestaurantController 
 {
     private RestaurantService restaurantService;
+
+    @Autowired
+    public RestaurantController(RestaurantService restaurantService) {
+        this.restaurantService = restaurantService;
+    }
     
-    @RequestMapping("api/restaurants")
+    @GetMapping("/findAll")
     public List<RestaurantDto> listRestaurants() 
     {
         return restaurantService.findAll();
